4b044980c5441604d8237e3c6a242941a6f48293
dborzenkov <danielspb@gmail.com>
2021-01-13 09:43:24 -0500

    init project

---
+++ src/test/java/guru/springframework/springjokeapp/SpringJokeappApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.springjokeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJokeappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
